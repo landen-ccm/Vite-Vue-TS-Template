f5a9e0454ce2dd99f9669a2888c53b8be416d5e2
nicknguyen-ccm <Nick.Nguyen@ccm.com>
2024-01-22 11:54:25 -0500

    Finished first task
    1. Setup project with clone and branch
    2. Created search bar and search button
    3. Made call to pokemon api through search bar and button
    4. Created a dropdown with designated sizes

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import './assets/main.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import 'primevue/resources/themes/lara-light-green/theme.css'
+import PrimeVue from 'primevue/config';
 const app = createApp(App)
 
 app.use(router)
 
+app.use(PrimeVue);
 app.mount('#app')
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,81 +1,53 @@
+<script setup lang="ts">
+import axios from 'axios'
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+import Dropdown from 'primevue/dropdown'
+
+const searchQuery = ref('')
+const pokemonURL = `https://pokeapi.co/api/v2/pokemon/`
+const pokemon = ref()
+const searchError = ref(false)
+const dropdownSizes = [25, 50, 100, 'All']
+const selectedSize = ref(25)
+
+function searchHandler() {
+  console.log(pokemonURL + searchQuery.value)
+  getPokemon(searchQuery.value)
+}
+
+function getPokemon(query: string) {
+  axios
+    .get(pokemonURL + query)
+    .then((result) => {
+      searchError.value = false
+      pokemon.value = result.data
+    })
+    .catch((error) => {
+      searchError.value = true
+      console.log(error)
+    })
+}
+
+onMounted(() => {
+  getPokemon('')
+})
+</script>
+
 <template>
-  <HelloWorld msg="Vite Vue TS" />
-
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
-
-    Vue's
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
-
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
-      >Cypress Component Testing</a
-    >.
-
-    <br />
-
-    More instructions are available in <code>README.md</code>.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
+  <div>
+    <i class="pi pi-check"></i>
+    <p v-if="searchError">Error: Invalid id or name</p>
+    <InputText
+      @keyup.enter="searchHandler"
+      v-model="searchQuery"
+      placeholder="enter a Pokemon name or id"
+      size="normal"
+    ></InputText>
+    <Button @click="searchHandler">search</Button>
+  </div>
+  <Dropdown :options="dropdownSizes" v-model="selectedSize"></Dropdown>
+  <div>
+    {{ pokemon }}
+  </div>
 </template>
