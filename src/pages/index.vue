eec9683f0db999dbb81588bed3b81fbf52022bca
kurt-ccm <156463193+kurt-ccm@users.noreply.github.com>
2024-01-22 11:37:54 -0500

    Imported new style structure from prime vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import Button from 'primevue/button';
 import InputText from 'primevue/inputtext';
+import Dropdown from 'primevue/dropdown';
+
 
 import { ref } from 'vue'
 
@@ -8,28 +10,50 @@ import 'primeicons/primeicons.css'
 import axios from 'axios';
 
 const searchParam = ref('');
+const selectedNum = ref();
+const pageSizes = [
+    25,
+    50,
+    100,
+    'all'
+]
 
 const getData = async () => {
     const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/');
     console.log(data);
 }
 
+const searchByName = async () => {
+    try {
+        const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${searchParam.value}`);
+        if (data.name) {
+            alert('success!')
+        }
+        console.log(data);
+    } catch (error) {
+        alert('No pokemon found with that name! Try again')
+        throw error
+    }
+
+}
+
 
 </script>
 
 <template>
-    <h2>hello</h2>
-    <div>
-        <i class="pi pi-check"></i>
-        <i class="pi pi-times"></i>
-        <span class="pi pi-search"></span>
-        <span class="pi pi-user"></span>
-    </div>
-    <div class="search-form">
-        <label for="username">Pokemon search</label>
-        <InputText v-model="searchParam" id="username" aria-describedby="username-help" />
-        <small id="username-help">Enter the complete name of a pokemon please</small>
-        <Button @click="getData">Search</Button>
+    <div class="card-container main-box">
+        <div class="search-form p-card">
+            <label for="username">Pokemon search</label>
+            <InputText @keyup.enter="searchByName" v-model="searchParam" id="username" aria-describedby="username-help" />
+            <small id="username-help">Enter the complete name of a pokemon please</small>
+            <Button @click="searchByName">Search</Button>
+        </div>
+        <div class="dropdown p-card card">
+            <!-- <h3>Pokemon display</h3> -->
+            <Dropdown v-model="selectedNum" :options="pageSizes" placeholder="How many pokemon per page?" />
+            <Button icon="pi pi-arrow-left" aria-label="Submit" />
+            <Button icon="pi pi-arrow-right" aria-label="Submit" />
+        </div>
     </div>
 </template>
 
@@ -40,4 +64,10 @@ const getData = async () => {
     max-width: 300px;
     gap: 2;
 }
+
+.main-box {
+    display: flex;
+    flex-direction: column;
+}
+
 </style>
