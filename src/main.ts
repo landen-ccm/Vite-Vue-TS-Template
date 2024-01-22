577918ca8826a60dc17c5044d66528d642399d39
Steven Chen <Steven.Chen@myccmortgage.com>
2024-01-22 12:14:03 -0500

    update task1

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,18 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import 'primeicons/primeicons.css'
+import 'primevue/resources/themes/lara-light-green/theme.css'
+import Dropdown from 'primevue/dropdown'
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+
+
+
 
 const app = createApp(App)
+app.use(PrimeVue)
 
 app.use(router)
 
