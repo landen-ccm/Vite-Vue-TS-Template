a3a244cf8f42e8dd35df0941649c3023479b4578
landen-ccm <109537809+landen-ccm@users.noreply.github.com>
2024-01-23 10:09:21 -0500

    Finish primevue setup

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import './assets/main.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
 import router from './router'
+import 'primevue/resources/themes/lara-light-green/theme.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(PrimeVue)
 
 app.mount('#app')
---
+++ src/pages/practice/pokemon/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Button from 'primevue/button'
+</script>
+
+<template>
+  <h1>Pokemon</h1>
+
+  <Button label="Click" />
+</template>
